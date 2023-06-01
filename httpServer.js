d12da90d11ee64f03c69f117a6c12aed3fa10403
joelaspe <joe.laspe@gmail.com>
2023-06-01 16:33:23 -0700

    Completed POST actions and catch all if bad route

--- httpServer.js
+++ httpServer.js
@@ -28,16 +28,59 @@ const server = http.createServer(function(req, res) {
                     res.end('Not Found');
                     //user entered a valid index /pets/index, return only the data at that index
                 } else {
-                    console.log("Made it to first line of else");
                     res.statusCode = 200;
                     res.setHeader('Content-Type', 'application/json');
                     res.end(JSON.stringify(parsed[parseInt(index)]));
                 }
                 
             }
-        })
+        });
+    } else if(req.method === 'POST' && req.url === '/pets') {
+        const petsPath = path.join(__dirname, 'pets.json');
+        fs.readFile(petsPath, 'utf-8', function(err, data) {
+            if(err) {
+                res.statusCode = 500;
+                console.error(err.message);
+            } else {
+                const parsed = JSON.parse(data);
+                //read the buffer from the HTTP PUT request body
+                let body = [];
+                let parsedBody;
+                req.on('data', (chunk) => {
+                    body.push(chunk);
+                } ).on('end', () => {
+                    parsedBody = JSON.parse(body);
+                    
+                    // check the parsedBody to make sure it meets minimum requirements of age=integer and nothing else empty
+    
+              
+                    if(isNaN(parseInt(parsedBody.age)) || !parsedBody['name'] || !parsedBody['kind'] || parsedBody['name'] === undefined || parsedBody['kind'] === undefined) {
+                        res.statusCode = 400;
+                        res.setHeader('Content-Type', 'text/plain');
+                        res.end('Bad Request'); 
+                    } else {
+                        parsed.push(parsedBody);
+                        console.log(parsed);
+                        fs.writeFile(petsPath, JSON.stringify(parsed), function(err) {
+                            if(err) {
+                                console.error(err);
+                            } else {
+                                res.statusCode = 200;
+                                res.setHeader('Content-Type', 'application/json');
+                                res.end(JSON.stringify(parsedBody));
+                            }
+                        });
+                    }
+                    
+                });
+                
+            } 
+        });
+    } else {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Not Found');
     } 
-    // other routes
 });
 
 //create a listener
