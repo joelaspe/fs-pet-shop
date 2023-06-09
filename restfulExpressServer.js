cccd27913c638e28c45a0de6b2d36e02a7a54088
joelaspe <joe.laspe@gmail.com>
2023-06-09 12:42:47 -0700

    Completed all RESTful routes connecting to postgreSQL server

--- restfulExpressServer.js
+++ restfulExpressServer.js
@@ -1,172 +1,151 @@
-//const fs = require('fs') //TODO: Remove references to fs
 import fs from 'fs';
-
-//const express = require('express')
 import express from 'express';
-//const path = require('path')
 import path from 'path';
-//const bodyParser = require('body-parser')
-import bodyParser from 'body-parser';
+
 const app = express()
 app.use(express.json());
-app.use(bodyParser.json());
-
 
 import pkg from 'pg' 
 const {Pool} = pkg;
 const client = new Pool({
     connectionString: 'postgres://petshopapi:petshopapi@localhost:5432/petshop'
-});
+}); 
 
 const PORT = 8000;
 const JSONFILE = 'pets.json'
 
 // GET ALL
-app.get('/pets', async function (req, res, next) {
-    const result = await client.query('SELECT * FROM pets');
-    console.log(result);
-    res.json(result);
-    /*
-    let petsPath = path.join(__dirname, JSONFILE)
-    fs.readFile(petsPath, "utf-8", function(err, data) {
-        if(err) {
-            next({ status: 500, type: 'text/plain', message: 'Internal Server Error', error: err});
-        } else {
-            let parsed = JSON.parse(data);
-            res.json(parsed)    
-        }
-    });
-    */
+app.get('/pets', async function (req, res) {
+    try {
+        const result = await client.query('SELECT * FROM pets');
+        res.json(result.rows);
+    } catch {
+        console.error(error.message);
+        res.status(500).type('text/plain').send('Internal Server Error');
+        console.error('Internal Server Error');
+        //next({ status: 500, type: 'text/plain', message: 'Internal Server Error', error: err});
+    }
 });
 // GET ONE
-app.get('/pets/:id', function (req, res, next) {
+app.get('/pets/:id', async function (req, res) {
     const id = req.params.id
-    let petsPath = path.join(__dirname, JSONFILE)
-        fs.readFile(petsPath, "utf-8", function(err, data) {
-        if(err) {
-            next({ status: 500, type: 'text/plain', message: 'Internal Server Error', error: err});
-        } else {
-            if (isNaN(parseInt(id))) {
-                res.status(400).type('text/plain').send('Bad Request');
+    if (isNaN(parseInt(id))) {
+        res.status(400).type('text/plain').send('Bad Request');
+    }
+    else {
+       try {
+            const result = await client.query('SELECT name, age, kind FROM pets WHERE id = $1', [id]);
+            // check if result is empty, if it is return 404 else give back the data
+            if(result.rows.length < 1){
+                res.status(404).type('text/plain').send('Not Found')
+            } else {
+                console.log(result.rows);
+                res.json(result.rows);
             }
-            else {
-                let parsed = JSON.parse(data);
-                if (!parsed[id]) {
-                    res.status(404).type('text/plain').send('Not Found')
-                }
-                else {
-                    
-                    res.json(parsed[id])
-                }
-            }
+            
+        } catch {
+            console.error(error.message);
+            res.status(500).type('text/plain').send('Internal Server Error');
+            console.error('Internal Server Error');
         }
-    });        
+       
+    }
 });
 // CREATE ONE
-app.post('/pets', function (req, res, next) {
-    const newPet = req.body
-    let petsPath = path.join(__dirname, JSONFILE)
-    fs.readFile(petsPath, "utf-8", function(err, data) {
-        if(err) {
-            next({ status: 500, type: 'text/plain', message: 'Internal Server Error', error: err});
+app.post('/pets', async function (req, res) {
+    const newPet = req.body;
+    console.log(newPet);
+    if(isNaN(parseInt(newPet.age)) || newPet['name'] === undefined || newPet['name'] === '' || newPet['kind'] === undefined || newPet['kind'] === '') {
+        res.type("text/plain").status(400).send('Bad Request');
+        
+    } else {
+        try {
+            const result = await client.query('INSERT INTO pets (name, age, kind) VALUES ($1, $2, $3)', [newPet.name, newPet.age, newPet.kind]);
+            console.log(result);
+            res.status(200).send(newPet);
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).type('text/plain').send('Internal Server Error');
+            console.error('Internal Server Error');
         }
-        else {
-            let parsed = JSON.parse(data)
-            if(isNaN(parseInt(newPet.age)) || newPet['name'] === undefined || newPet['name'] === '' || newPet['kind'] === undefined || newPet['kind'] === '') {
-                res.type("text/plain")
-                next({ status: 400, message: 'Bad Request'})
-            }
-            else {
-                parsed.push(newPet);
-                fs.writeFile(petsPath, JSON.stringify(parsed), function(err) {
-                    if(err) {
-                        next({ status: 500, type: 'text/plain', message: 'Internal Server Error', error: err});
-                    } else {
-                        res.status(200).send(newPet);
-                    }
-                });
-            }
-        }
-    });
+    }
 });
 // DELETE ONE
-app.delete('/pets/:id', function (req, res, next) {
-    const id = req.params.id;
-    let petsPath = path.join(__dirname, JSONFILE)
-        fs.readFile(petsPath, "utf-8", function(err, data) {
-        if(err) {
-            next({ status: 500, type: 'text/plain', message: 'Internal Server Error', error: err});
-        } else {
-            if (isNaN(parseInt(id))) {
-                res.status(400).type('text/plain').send('Bad Request');
+app.delete('/pets/:id', async function (req, res) {
+    const id = req.params.id
+    if (isNaN(parseInt(id))) {
+        res.status(400).type('text/plain').send('Bad Request');
+    }
+    else {
+        try {
+            const deletedPet = await client.query('SELECT name, age, kind FROM pets WHERE id = $1', [id]);
+            const result = await client.query('DELETE FROM pets WHERE id = $1', [id]);
+            if(result.rowCount === 0){
+                res.status(404).type('text/plain').send('Not Found')
+            } else {
+                res.status(200).send(deletedPet.rows[0]);
             }
-            else {
-                let parsed = JSON.parse(data);
-                if (!parsed[id]) {
-                    res.status(404).type('text/plain').send('Not Found')
-                }
-                else {
-                    let deletedPet = parsed[id];          
-                    // delete from parsed the requested object
-                    parsed.splice(id,1);
-                    fs.writeFile(petsPath, JSON.stringify(parsed), function(err) {
-                        if(err) {
-                            next({ status: 500, type: 'text/plain', message: 'Internal Server Error', error: err});
-                        } else {
-                            res.status(200).send(deletedPet);
-                        }
-                    });
-                }
-            }
+            
+        } catch {
+            console.error(error.message);
+            res.status(500).type('text/plain').send('Internal Server Error');
+            console.error('Internal Server Error');
         }
-    });        
+       
+    }
 });
 // UPDATE ONE
-app.patch('/pets/:id', function (req, res, next) {
-    const id = req.params.id;
+app.patch('/pets/:id', async function (req, res) {
     const updatePetInfo = req.body;
-    let petsPath = path.join(__dirname, JSONFILE)
-        fs.readFile(petsPath, "utf-8", function(err, data) {
-        if(err) {
-            next({ status: 500, type: 'text/plain', message: 'Internal Server Error', error: err});
+    const id = req.params.id
+    if (isNaN(parseInt(id))) {
+        res.status(400).type('text/plain').send('Bad Request');
+    }
+    const keyList = Object.keys(updatePetInfo);
+    let SQLString = 'UPDATE pets SET '; // will build on this later
+    let inputs = '';
+    for(let i = 0; i < keyList.length; i++) {
+        // filter the inputted data. If the key is anything other than 'age' add single quotes ' ' around the data.
+        if(updatePetInfo[keyList[i]] === undefined || updatePetInfo[keyList[i]] === '') {
+            res.status(400).type('text/plain').send('Bad Request');
+            return;
+        }
+        if(keyList[i] !== 'age'){
+            updatePetInfo[keyList[i]] = '\'' + updatePetInfo[keyList[i]] + '\''
         } else {
-            // check to make sure id is actually a valid number
-            if (isNaN(parseInt(id))) {
+            if (isNaN(parseInt(updatePetInfo[keyList[i]]))) {
                 res.status(400).type('text/plain').send('Bad Request');
-            }
-            else {
-                let parsed = JSON.parse(data);
-                
-                for(key in updatePetInfo) {
-                    if(key !== undefined && updatePetInfo[key] !== '' && parsed[id][key]){
-                        if(key === 'age' && isNaN(parseInt(updatePetInfo[key])))
-                        {
-                            res.status(400).type('text/plain').send('Bad Request');
-                            return;
-                        } else {
-                            parsed[id][key] = updatePetInfo[key];
-                        }
-                    }
-                }
-                fs.writeFile(petsPath, JSON.stringify(parsed), function(err) {
-                    if(err) {
-                        next({ status: 500, type: 'text/plain', message: 'Internal Server Error', error: err});
-                    } else {
-                        res.status(200).send(parsed[id]);
-                    }
-                });
+                return;
             }
         }
-    });        
+        // now build onto the SQL string adding commas if not last
+        if(i < keyList.length-1){
+            inputs += keyList[i] + ' = ' + updatePetInfo[keyList[i]] + ', ';
+        } else { //last item, no comma required
+            inputs += keyList[i] + ' = ' + updatePetInfo[keyList[i]];
+        }
+    }
+    SQLString += inputs;
+    SQLString += ' WHERE id = ' + '\'' + id + '\'';
+    try {
+        const result = await client.query(SQLString);
+        if(result.rowCount === 0){
+            res.status(404).type('text/plain').send('Not Found')
+        } else {
+            
+            const updatedRecord = await client.query('SELECT name, age, kind FROM pets WHERE id = $1', [id]);    
+            res.status(200).send(updatedRecord.rows[0]); 
+        }
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).type('text/plain').send('Internal Server Error');
+        console.error('Internal Server Error'); 
+    }
 });
 
 app.use((req, res, next) => {
     res.status(404).type('text/plain').send('Not Found');
 })
-
-app.use((err, req, res, next) => {
-    res.status(err.status).type(err.type).send(err.message);
-});
 
 app.listen(PORT, function() {
     console.log(`Listening on port ${PORT}`)
